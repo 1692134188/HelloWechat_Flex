87ea853276483c758b7ab6c8c8643b7f8bb263bf
逍遥小天狼 <1692134188@qq.com>
2020-05-14 18:27:05 +0800

    基础语法 index

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,136 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    guobaoList: [
+      {
+        pic_url: "/static/index/1.jpg",
+        isLike: true,
+        ownerName: "盖聂",
+        ownerFansNums: 500,
+        ownerForm: '秦时明月'
+      },
+      {
+        pic_url: "/static/index/2.jpg",
+        isLike: true,
+        ownerName: "乔峰",
+        ownerFansNums: 600,
+        ownerForm: '天龙八部'
+      },
+      {
+        pic_url: "/static/index/3.jpg",
+        isLike: true,
+        ownerName: "Harry Petter",
+        ownerFansNums: 50,
+        ownerForm: '哈利 波特'
+      },
+      {
+        pic_url: "/static/index/4.jpg",
+        isLike: false,
+        ownerName: "伏地魔",
+        ownerFansNums: 600,
+        ownerForm: '哈利 波特'
+      },
+      {
+        pic_url: "/static/index/5.jpg",
+        isLike: true,
+        ownerName: "琼恩 雪诺",
+        ownerFansNums: 50,
+        ownerForm: '冰与火'
+      },
+      {
+        pic_url: "/static/index/6.jpg",
+        isLike: false,
+        ownerName: "夜王",
+        ownerFansNums: 600,
+        ownerForm: '权力的游戏'
+      },
+      {
+        pic_url: "/static/index/7.jpg",
+        isLike: true,
+        ownerName: "苏轼",
+        ownerFansNums: 50,
+        ownerForm: '宋'
+      },
+      {
+        pic_url: "/static/index/8.jpg",
+        isLike: true,
+        ownerName: "李白",
+        ownerFansNums: 600,
+        ownerForm: '唐'
+      },
+      {
+        pic_url: "/static/index/9.jpg",
+        isLike: true,
+        ownerName: "秦始皇",
+        ownerFansNums: 50,
+        ownerForm: '秦'
+      },
+      {
+        pic_url: "/static/index/10.jpg",
+        isLike: true,
+        ownerName: "汉武帝",
+        ownerFansNums: 60,
+        ownerForm: '汉'
+      }
+    ]
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  // onLoad: function () {
+  //   if (app.globalData.userInfo) {
+  //     this.setData({
+  //       userInfo: app.globalData.userInfo,
+  //       hasUserInfo: true
+  //     })
+  //   } else if (this.data.canIUse) {
+  //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+  //     // 所以此处加入 callback 以防止这种情况
+  //     app.userInfoReadyCallback = res => {
+  //       this.setData({
+  //         userInfo: res.userInfo,
+  //         hasUserInfo: true
+  //       })
+  //     }
+  //   } else {
+  //     // 在没有 open-type=getUserInfo 版本的兼容处理
+  //     wx.getUserInfo({
+  //       success: res => {
+  //         app.globalData.userInfo = res.userInfo
+  //         this.setData({
+  //           userInfo: res.userInfo,
+  //           hasUserInfo: true
+  //         })
+  //       }
+  //     })
+  //   }
+  // },
+  onLoad: function (options) {
+
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  likeClick: function (e) {
+    // 提前准备好对象
+    let index = e.currentTarget.dataset['index'];
+    var item = this.data.guobaoList[index]
+    console.log(item);
+    item.isLike = !item.isLike;
+    // 依旧是根据index获取数组中的对象
+    var key = "guobaoList[" + index + "]"
+    this.setData({
+      // 这里使用键值对方式赋值
+      key: item
+    }, function () {this.onReady() })
+    
   }
 })
